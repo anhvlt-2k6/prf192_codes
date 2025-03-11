6c30697ba522ef284d62e327c4652179f3d0cc2b
anhvlt-2k6 <anhvlt.2006@outlook.com>
2025-03-11 21:59:00 +0700

    Create main.c

---
+++ average_of_odds/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int n, inp, sum_odds = 0, count_odds = 0;
+	
+	scanf("%d", &n);
+	
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &inp);
+		if (inp % 2 != 0) {
+			sum_odds += inp;
+			count_odds += 1;
+		}
+	}
+	
+	printf("%.3lf", (double)sum_odds/count_odds);
+	
+	return 0;
+}
