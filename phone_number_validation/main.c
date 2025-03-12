8e96196e22cba34aa715bc9648daefef3697b2c9
anhvlt-2k6 <anhvlt.2006@outlook.com>
2025-03-12 11:11:48 +0700

    Update main.c

--- phone_number_validation/main.c
+++ phone_number_validation/main.c
@@ -5,10 +5,15 @@
 #include <ctype.h>
 
 bool mobile_phone_number(const char *pon_num) {
-	size_t len = strlen(pon_num);
+	int len = strlen(pon_num);
+	
+	// check for if the first char is either 0
 	if (pon_num[0] != '0') return false;
+	
+	// check for if the len is not 10 or 11
 	if (len != 10 && len != 11) return false;
-	for (size_t i = 1; i < len; i++) {
+	
+	for (int i = 1; i < len; i++) {
 		if (!isdigit((unsigned char)pon_num[i])) return false;
 	}
 	
@@ -16,30 +21,42 @@ bool mobile_phone_number(const char *pon_num) {
 }
 
 bool telephone_number(const char *pon_num) {
-	size_t len = strlen(pon_num);
+	int len = strlen(pon_num);
+	
 	int close_paren_loc = -1;
+	
+	// if len < 6 its not phone num
 	if (len < 6) return false;
+	
+	// if the start is not ( is not tele
 	if (pon_num[0] != '(') return false;
 	
-	for (int i = 0; i < (int)len; i++) {
+	// check for the close paren
+	for (int i = 0; i < len; i++) {
 		if (pon_num[i] == ')') {
 			close_paren_loc = i;
 			break;
 		}
+		
+		if (!isdigit((unsigned char)pon_num[i])) return false;
+
 	}
 	
+	// no close paren detected
 	if (close_paren_loc == -1) return false;
-	if (close_paren_loc - 1 < 2 || close_paren_loc - 1 > 4) return false;
-	if ((int)len - (close_paren_loc + 1) != 7) return false;
 	
-	for (int j = 1; j < (int)len; j++) {
-        if (j == close_paren_loc) {
-            continue;
-        }
-        if (!isdigit((unsigned char)pon_num[j])) {
+	// check inside (...)
+	if (close_paren_loc - 1 < 2 || close_paren_loc - 1 > 4) return false;
+	
+	// check after the (...)
+	if (len - (close_paren_loc + 1) != 7) return false;
+	
+	// check all other char except ()
+	for (int i = close_paren_loc + 1; i < len; i++) {
+        if (!isdigit((unsigned char)pon_num[i])) {
             return false;
         }
-	}
+    }
 	
 	return true;
 }
@@ -47,18 +64,17 @@ bool telephone_number(const char *pon_num) {
 int main() {
 	
 	int n, count_valid = 0;
-	char *pon_num = malloc(64 * sizeof(char));;
+	char *pon_num;
 	
 	scanf("%d", &n);
 	
 	for (int i = 0; i < n; i++) {
+		pon_num = malloc(64 * sizeof(char));
 		scanf("%63s", pon_num);
 		
 		if (mobile_phone_number(pon_num) || telephone_number(pon_num)) {
-			count_valid += 1;
+			count_valid++;
 		}
-		
-		free(pon_num);
 	}
 	
 	printf("%d", count_valid);
